4c1180b6e6ef1496bcbc717bf3dda9d2c04291c8
[Mark Zhang] <[markjustinzhang@gmail.com]>
2024-12-02 17:29:40 -0500

    created window and label

--- mp3_player.py
+++ mp3_player.py
@@ -1,1 +1,15 @@
 import tkinter as tk
+
+
+class MusicPlayer:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("800x600")
+
+        self.label = tk.Label(self.root, text="MP3 Player", font=("Arial", 24))
+        self.label.pack(padx=20, pady=20)
+
+        self.root.mainloop()
+
+
+MusicPlayer()
