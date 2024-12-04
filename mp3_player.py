50ada818890b4f8c8563fa12cb3b7d88951432e1
[Mark Zhang] <[markjustinzhang@gmail.com]>
2024-12-04 09:15:00 -0500

    added title

--- mp3_player.py
+++ mp3_player.py
@@ -4,6 +4,7 @@ import tkinter as tk
 class MusicPlayer:
     def __init__(self):
         self.root = tk.Tk()
+        self.root.title("Amazing MP3 Player")
         self.root.geometry("800x600")
 
         self.label = tk.Label(self.root, text="MP3 Player", font=("Arial", 24))
