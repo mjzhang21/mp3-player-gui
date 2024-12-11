bc6349eeac91deb9323ac709b98b06ca3aefbbf8
[Mark Zhang] <[markjustinzhang@gmail.com]>
2024-12-10 20:45:00 -0500

    organized code

--- mp3_player.py
+++ mp3_player.py
@@ -18,7 +18,6 @@ class MusicPlayer:
         self.my_menu = tk.Menu(self.root)
         self.root.config(menu=self.my_menu)
         self.file_menu = tk.Menu(self.my_menu, tearoff=0)
-
         self.my_menu.add_cascade(label="File", menu=self.file_menu)
         self.file_menu.add_command(label="Open", command=self.add_song)
         self.file_menu.add_command(label="Exit", command=quit)
@@ -34,21 +33,20 @@ class MusicPlayer:
         self.root.columnconfigure(1, minsize=260)
 
         self.song_listbox = tk.Listbox(self.root, bg="green", fg="white", font=(14))
+        self.song_listbox.grid(row=0, column=1, rowspan=2, sticky="nsew")
 
         self.control_frame = tk.Frame(self.root)
+        self.control_frame.grid(row=1, column=0, sticky="ns")
 
-        self.btn_forward = tk.Button(self.control_frame, image=self.forward)
         self.btn_back = tk.Button(self.control_frame, image=self.back)
         self.btn_play = tk.Button(
             self.control_frame, image=self.play, command=self.play_music
         )
+        self.btn_forward = tk.Button(self.control_frame, image=self.forward)
 
         self.btn_back.grid(row=0, column=0, sticky="ew", padx=15)
         self.btn_play.grid(row=0, column=1, sticky="ew", padx=15)
         self.btn_forward.grid(row=0, column=2, sticky="ew", padx=15)
-
-        self.song_listbox.grid(row=0, column=1, rowspan=2, sticky="nsew")
-        self.control_frame.grid(row=1, column=0, sticky="ns")
 
     def add_song(self):
         song_path = tk.filedialog.askopenfilename(
@@ -67,6 +65,8 @@ class MusicPlayer:
             pygame.mixer.music.play()
 
 
-root = tk.Tk()
-MusicPlayer(root)
-root.mainloop()
+if __name__ == "__main__":
+
+    root = tk.Tk()
+    MusicPlayer(root)
+    root.mainloop()
