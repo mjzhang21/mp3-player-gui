3ab9abef770b37109d81bcaa1867fa2d1769a495
mjzhang21 <markjustinzhang@gmail.com>
2024-12-12 21:24:04 -0500

    minor changes

--- mp3_player.py
+++ mp3_player.py
@@ -7,7 +7,7 @@ class MusicPlayer:
     def __init__(self, root):
         self.root = root
         self.root.title("Amazing MP3 Player")
-        self.root.geometry("700x500")
+        self.root.geometry("800x600")
 
         self.song_dict = {}
         self.path = ""
@@ -27,12 +27,13 @@ class MusicPlayer:
         self.forward = tk.PhotoImage(file="images/forward-button.png")
         self.back = tk.PhotoImage(file="images/back-button.png")
 
-        self.root.rowconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=4)
         self.root.rowconfigure(1, minsize=100, weight=0)
-        self.root.columnconfigure(0, weight=1)
-        self.root.columnconfigure(1, minsize=260)
+        self.root.columnconfigure(0, weight=4)
+        self.root.columnconfigure(1, minsize = 200, weight = 1)
+        
 
-        self.song_listbox = tk.Listbox(self.root, bg="green", fg="white", font=(14))
+        self.song_listbox = tk.Listbox(self.root, bg="gray43", fg="white", font = ('Times', 18), width = 8)
         self.song_listbox.grid(row=0, column=1, rowspan=2, sticky="nsew")
 
         self.control_frame = tk.Frame(self.root)
@@ -50,10 +51,10 @@ class MusicPlayer:
 
     def add_song(self):
         song_path = tk.filedialog.askopenfilename(
-            title="Select a file", filetypes=(("mp3 Files", "*.mp3"),)
+            title="Select a file", filetypes=(("mp3 Files", "*.mp3"),),initialdir = r"C:\Users\piefo\Music\playlists"
         )
         if song_path:
-            song_name = song_path.split("/")[-1].split(".")[0]
+            song_name = song_path.split("/")[-1].split(".")[:-1]
             self.song_dict[self.count] = [song_name, song_path]
             self.song_listbox.insert(self.count, song_name)
             self.path = song_path
